4e0a16ff87fe38c9b2b085836b022fcbcd638dc2
Yi Xie <dnatag@gmail.com>
2017-01-14 22:37:44 -0500

    fountainjs generated

---
+++ wallaby.js
@@ -0,0 +1,48 @@
+const wallabyWebpack = require('wallaby-webpack');
+const angularTemplatePreprocessor = require('wallaby-ng-html2js-preprocessor');
+const wallabyPostprocessor = wallabyWebpack({});
+
+module.exports = function (wallaby) {
+  return {
+    files: [
+      {
+        pattern: 'src/**/*.html',
+        load: false
+      },
+      {
+        pattern: 'src/**/*.css',
+        load: false
+      },
+      {
+        pattern: 'src/**/*.ts',
+        load: false
+      },
+      {
+        pattern: 'src/**/*.spec.ts',
+        ignore: true
+      }
+    ],
+
+    tests: [{
+      pattern: 'src/**/*.spec.ts',
+      load: false
+    }],
+
+    testFramework: 'jasmine',
+
+    preprocessors: {
+      'src/**/*.html': function (file) {
+        return angularTemplatePreprocessor.transform(file, {stripPrefix:'src/'})
+      }
+    },
+
+    postprocessor: wallabyPostprocessor,
+
+    setup: function () {
+      // required to trigger test loading
+      window.__moduleBundler.loadTests();
+    },
+
+    debug: true
+  };
+};
